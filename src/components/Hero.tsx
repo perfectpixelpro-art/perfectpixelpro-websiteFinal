07f360c423ec893d9e11a149a29bd9a15666c7df
utkarsh <perfectpixelpro@gmail.com>
2026-05-28 16:25:39 +0530

    testing for github in utkarsh pc

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        Hello, TypeScript + React 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
